d6843978b096f2930f83ccf23ebeab487eb25e4d
Sansc0lD <148742241+Sansc0lD@users.noreply.github.com>
2025-09-09 12:39:30 +0800

    Update swap method
    semoga bener

--- Queue/manualQueue.java
+++ Queue/manualQueue.java
@@ -1,11 +1,14 @@
 package Queue;
+
 class Node {
     char data;    
-    Node next;    
+    Node next;
+    Node prev;
 
     public Node(char data) {
         this.data = data;
         this.next = null;
+        this.prev = null;
     }
 }
 
@@ -38,7 +41,7 @@ class manualQueue {
     public char keluar() {
         if (isEmpty()) {
             System.out.println("Antrian kosong, gada yg bisa keluar");
-            return '\0'; 
+            return '\0';
         }
 
         char dataYangKeluar = front.data;
@@ -48,6 +51,8 @@ class manualQueue {
 
         if (front == null) {
             rear = null;
+        } else {
+            front.prev = null;
         }
 
         return dataYangKeluar;
@@ -59,6 +64,34 @@ class manualQueue {
             return '\0';
         }
         return front.data;
+    }
+
+    public void swap(char data1, char data2) {
+        if (data1 == data2) return; //klo isi sama gausa ditukar
+
+        Node node1 = null, node2 = null;
+        Node penunjuk = front;
+
+        while (penunjuk != null) {
+            if (penunjuk.data == data1) {
+                node1 = penunjuk;
+            }
+            if (penunjuk.data == data2) {
+                node2 = penunjuk;
+            }
+            penunjuk = penunjuk.next;
+        }
+
+        if (node1 == null || node2 == null) {
+            System.out.println("Gagal swap, nodenya gada");
+            return;
+        }
+        
+        // Cukup tukar datanya saja untuk simplisitas
+        char temp = node1.data;
+        node1.data = node2.data;
+        node2.data = temp;
+        System.out.println("Data '" + data1 + "' dan '" + data2 + "' ditukar.");
     }
 
     public void cetakAntrian() {
@@ -92,16 +125,28 @@ class manualQueue {
 
         antrianHuruf.tambah('C');
         antrianHuruf.cetakAntrian();
+
+        antrianHuruf.tambah('D');
+        antrianHuruf.cetakAntrian();
+
+        antrianHuruf.tambah('E');
+        antrianHuruf.cetakAntrian();
         
         System.out.println("Karakter di depan: '" + antrianHuruf.lihat() + "'");
+
+        System.out.println("Ngeswap 'B' dan 'D': ");
+        antrianHuruf.swap('B', 'D');
+        antrianHuruf.cetakAntrian();
         
         antrianHuruf.keluar(); //a keluar
         antrianHuruf.cetakAntrian();
         
         System.out.println("Karakter di depan skrg: '" + antrianHuruf.lihat() + "'");
 
-        antrianHuruf.keluar(); //b keluar
-        antrianHuruf.keluar(); //c keluar
+        antrianHuruf.keluar();
+        antrianHuruf.keluar();
+        antrianHuruf.keluar();
+        antrianHuruf.keluar();
 
         antrianHuruf.cetakAntrian(); //cetak pas kosong
         
