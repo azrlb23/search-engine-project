1e9e76155d90c87de6df43dda6d30eef6dd40fe1
perhanjay <11241056@student.itk.ac.id>
2025-09-27 00:35:24 +0800

    revision on simple sorting algoritm

--- Sorting/SimpleSorting.java
+++ Sorting/SimpleSorting.java
@@ -1,5 +1,6 @@
 import jdk.swing.interop.SwingInterOpUtils;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class SimpleSorting extends DoubleLinkedList{
@@ -46,15 +47,6 @@ public class SimpleSorting extends DoubleLinkedList{
         }
     }
 
-    public void insertAfter(Node nodeAfter, Node toInsert){
-        Node temp = nodeAfter.next;
-
-        nodeAfter.next = toInsert;
-        toInsert.prev = nodeAfter;
-        temp.prev = toInsert;
-        toInsert.next = temp;
-    }
-
     public Node removeFrom(Node nodeX){
         nodeX.prev.next = nodeX.next;
         if(nodeX.next != null){
@@ -63,13 +55,6 @@ public class SimpleSorting extends DoubleLinkedList{
             tail = nodeX.prev;
         }
         return nodeX;
-    }
-
-    //TESTER DEBUGGER
-    public static void insertButUnRand(SimpleSorting list, int length){
-        for(int i = 0; i < length; i++){
-            list.insertAtEnd(i);
-        }
     }
 
     public void insertionSort(){
@@ -133,118 +118,91 @@ public class SimpleSorting extends DoubleLinkedList{
         tail = null;
     }
 
-    public boolean isSorted() {
-        if (head == null || head.next == null) {
-            return true; // Daftar kosong atau satu elemen dianggap terurut
-        }
-
-        Node current = head;
-        while (current.next != null) {
-            if (current.value > current.next.value) {
-                return false;
-            }
-            current = current.next;
-        }
-        return true;
-    }
-
-    public void shuffle() {
-        if (getLength() <= 1) {
-            return;
-        }
-
-        // 1. Salin data dari linked list ke ArrayList
-        List<Integer> dataList = new ArrayList<>();
-        Node current = head;
+    public SimpleSorting cloneList(){
+        SimpleSorting cloned = new SimpleSorting();
+        Node current = this.head;
         while (current != null) {
-            dataList.add(current.value);
+            cloned.insertAtEnd(current.value);
             current = current.next;
         }
-
-        // 2. Kocok ArrayList menggunakan Collections.shuffle()
-        Collections.shuffle(dataList);
-
-        // 3. Salin kembali data yang sudah dikocok ke linked list
-        current = head;
-        for (Integer data : dataList) {
-            current.value = data;
-            current = current.next;
-        }
-    }
-
-    public void bogoSort() {
-        long attempts = 0;
-        while (!isSorted()) {
-            shuffle();
-            attempts++;
-            System.out.println("Percobaan ke-" + attempts + ": ");
-            traverseForward();
-        }
-        System.out.println("\nDaftar berhasil diurutkan setelah " + attempts + " kali percobaan!");
+        return cloned;
     }
 
     public static void main(String[] args) {
         SimpleSorting list = new SimpleSorting();
+        SimpleSorting list2;
         Scanner sc = new Scanner(System.in);
         int option = -1;
+        int initialSetup = -1;
 
-        while(option != 0){
+        System.out.println("Initial Menu\nChoose Data Type: ");
+        System.out.println("1. Linked List");
+        System.out.println("2. Array");
+        System.out.print(": ");
+        initialSetup = sc.nextInt();
+        while (option != 0) {
             System.out.println("\u001B[42m  \u001B[30mSIMPLE SORTING MENUS:  \u001B[0m");
             System.out.println("6. Random add");
             System.out.println("7. Bubble Sort");
             System.out.println("8. Insertion Sort");
             System.out.println("9. Selection Sort");
             System.out.println("10. Clear List");
-            System.out.println("11. Bogo Sort");
+            System.out.println("11. Print list (Original)");
             System.out.println("\u001B[31m0. Exit\u001B[0m");
             System.out.print(": ");
             option = sc.nextInt();
 
-            switch(option){
+            switch (option) {
                 case 6:
                     System.out.print("Length To Insert: ");
                     int length = sc.nextInt();
                     Random r = new Random();
-                    for (int i = 0; i < length;i++){
+                    for (int i = 0; i < length; i++) {
                         list.insertAtEnd(r.nextInt(1000));
                     }
                     list.traverseForward();
                     break;
                 case 7:
+                    list2 = list.cloneList();
                     System.out.println("\u001B[32mSORTING\u001B[0m");
                     long startime = System.nanoTime();
-                    list.bubbleSort();
+                    list2.bubbleSort();
                     long endtime = System.nanoTime();
                     System.out.println("\u001B[32mSORTING DONE\u001B[0m");
                     System.out.println("\nRESULT");
-                    list.traverseForward();
+                    list2.traverseForward();
                     System.out.print("\u001B[32m TIME ELAPSED:\u001B[0m ");
                     double durationTime = (endtime - startime) / 1_000_000.0;
-                    System.out.println("\u001B[31m"+ durationTime + "\u001B[0m ms");
+                    System.out.println("\u001B[31m" + durationTime + "\u001B[0m ms");
+                    list2.clearList();
                     break;
                 case 8:
+                    list2 = list.cloneList();
                     System.out.println("\u001B[32mSORTING\u001B[0m");
                     long startime2 = System.nanoTime();
-                    list.insertionSort();
+                    list2.insertionSort();
                     long endtime2 = System.nanoTime();
                     System.out.println("\u001B[32mSORTING DONE\u001B[0m");
                     System.out.println("\nRESULT");
-                    list.traverseForward();
+                    list2.traverseForward();
                     System.out.print("\u001B[32m TIME ELAPSED:\u001B[0m ");
                     double durationTime2 = (endtime2 - startime2) / 1_000_000.0;
-                    System.out.println("\u001B[31m"+ durationTime2 + "\u001B[0m ms");
+                    System.out.println("\u001B[31m" + durationTime2 + "\u001B[0m ms");
+                    list2.clearList();
                     break;
                 case 9:
+                    list2 = list.cloneList();
                     System.out.println("\u001B[32mSORTING\u001B[0m");
                     long startime3 = System.nanoTime();
-                    list.selectionSort();
+                    list2.selectionSort();
                     long endtime3 = System.nanoTime();
                     System.out.println("\u001B[32mSORTING DONE\u001B[0m");
                     System.out.println("\nRESULT");
-                    list.traverseForward();
+                    list2.traverseForward();
                     double durationTime3 = (endtime3 - startime3) / 1_000_000.0;
                     System.out.print("\u001B[32m TIME ELAPSED:\u001B[0m ");
-                    System.out.println("\u001B[31m"+ durationTime3 + "\u001B[0m ms");
+                    System.out.println("\u001B[31m" + durationTime3 + "\u001B[0m ms");
+                    list2.clearList();
                     break;
                 case 10:
                     System.out.println("CLEARING");
@@ -253,21 +211,11 @@ public class SimpleSorting extends DoubleLinkedList{
                     list.traverseForward();
                     break;
                 case 11:
-                    System.out.println("BOGO SORT");
-                    long startime4 = System.nanoTime();
-                    list.bogoSort();
-                    long endtime4 = System.nanoTime();
-                    System.out.println("\u001B[32mSORTING DONE\u001B[0m");
-                    System.out.println("\nRESULT");
                     list.traverseForward();
-                    double durationTime4 = (endtime4 - startime4) / 1_000_000.0;
-                    System.out.print("\u001B[32m TIME ELAPSED:\u001B[0m ");
-                    System.out.println("\u001B[31m"+ durationTime4 + "\u001B[0m ms");
                     break;
                 case 0:
                     break;
             }
         }
-
     }
 }
